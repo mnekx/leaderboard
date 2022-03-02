b54485e063189d197fc69ed79a166ff57d8f5b08
Mnemba Chambuya <karimnembac@gmail.com>
2022-03-02 14:26:51 +0300

    implement the refresh button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import './style.css';
 import ListItemsComponent from './container_component.js';
+import { getData } from './services.js';
 
-const listItemsComponent = new ListItemsComponent([]);
+const listItemsComponent = new ListItemsComponent();
+
+getData().then((data) => {
+  const leaders = data.result.map((item) => ({
+    name: item.user,
+    score: item.score,
+  }));
+  listItemsComponent.setList(leaders);
+});
 
 const addingBtn = document.querySelector('#add-btn');
 
 const addingForm = document.querySelector('#add-form');
 
-listItemsComponent.returnComponent();
+const refreshBtn = document.querySelector('#refresh');
+
+listItemsComponent.renderComponent();
 
 addingForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -19,4 +30,8 @@ addingBtn.addEventListener('click', () => {
   listItemsComponent.addItem(nameInput.value, scoreInput.value);
   nameInput.value = '';
   scoreInput.value = '';
+});
+
+refreshBtn.addEventListener('click', () => {
+  listItemsComponent.renderComponent();
 });
